007e39213f9b98f9dfffb104f2907f8d12978be7
Robin Houston <robin.houston@gmail.com>
2011-07-22 22:59:51 +0100

    Add some documentation to the find_path and find_library calls

--- CMakeModules/FindGMP.cmake
+++ CMakeModules/FindGMP.cmake
@@ -1,3 +1,3 @@
-find_path(GMP_INCLUDE_DIR NAMES gmp.h)
-find_library(GMP_LIBRARY NAMES gmp libgmp)
+find_path(GMP_INCLUDE_DIR NAMES gmp.h DOC "The directory containing the GMP header files")
+find_library(GMP_LIBRARY NAMES gmp libgmp DOC "The directory containing the GMP library")
 set(GMP_LIBRARIES ${GMP_LIBRARY})
